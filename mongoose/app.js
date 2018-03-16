1b028d6cf87220138c38097fbcf5fab3cd794fb5
YanTing1111 <1178170499@qq.com>
2018-03-16 17:01:59 +0800

    '多表链接查询'

--- mongoose/app.js
+++ mongoose/app.js
@@ -1,40 +1,106 @@
 const Koa = require('koa')
 const app = new Koa()
+const validation = require('./validation');
 const mongoose = require('mongoose')
-const mongoUrl = `mongodb://127.0.0.1:27017/vueblog`  //连接数据库
+const mongoUrl = `mongodb://127.0.0.1:27017/vueblog`
 const Schema = mongoose.Schema
-//进行model层的抽象
+// model 层抽像
 const UserSchema = new Schema({
-    name:{
-        type:String,
-        required:true,
-        index:true
-    }
+  name: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 8,
+    index: true,
+    unique: true
+  },
+  age: {
+    type: Number,
+    min: 18,
+    max: 30,
+    required: true
+  },
+  sex: {
+    type: String,
+    enum: {
+      values: ['male', 'female'],
+      message: '`${PATH}`是`${VALUE}`,您必须确认您的性别!'
+    },
+    required: true
+  },
+  phone: {
+    type: String,
+    validate: {
+      validator: validation.phone,
+      message: '`{PATH}` 必须是有效的11位手机号码!'
+    },
+    required: true
+  }
 })
-mongoose.model('User',UserSchema)
+mongoose.model('User', UserSchema)
+
+const ArticleSchema = new Schema({
+  title: {
+    type: String,
+    index: true
+  },
+  by:{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
+})
+mongoose.model('Article', ArticleSchema)
 
 mongoose.Promise = global.Promise
-mongoose.connection.openUri(mongoUrl).once('open',async() => {
+mongoose.connection
+  .openUri(mongoUrl)
+  .once('open', async () => {
     console.log('database connect success')
-}).on('error',(error) => {
-    console.warn('database connect fail',error)
-})
+  })
+  .on('error', (error) => {
+    console.wran('database connect fail',
+     error)
+  })
+const main = async (ctx) => {
+  // const id = ctx.req.id
+  // const ArticleModel = mongoose.model('Article')
+  // try {
+  //   const article = await ArticleModel.findById('5aab7b5db7a7f9fc8b7523d2')
+  //   .populate('by')
+  //   .exec()
+  //   console.log(article);
+  //   const str = `文章标题：${article.title},
+  //   作者：${article.by.name}`
+  //   ctx.body = str;
+  // } catch(e) {
+  //   console.log(e)
+  //   ctx.body = '查询失败';
+  // }
+  
 
-const main = async(ctx) => {
-    const User = mongoose.model('User')
-    //新建一个user过程：new User类,成为内存中与Schema规则一致的对象 ->
-    const user = new User({
-        name:'小田'
-    })
-    //接口存取是花时间的 IO
-    // 数据库读取，文件读取，网络请求
-    
-    try{
-        await user.save()
-        ctx.body = 'ok, 保存成功了'
-    }catch(e){
-        ctx.body = 'sorry，保存失败了'
-    }
+  const User  = mongoose.model('User')
+  // // 新建一个user User类
+  // // 内存中与Schema规则一致的对象 
+  const user = new User({
+    name: '小花',
+    age:18,
+    phone:'15779899276',
+    sex:'male'
+  })
+  
+  // 接口存取是花时间的 IO 
+  // 数据库读取， 文件读取， 网络请求
+  
+  try {
+    await user.save()
+    ctx.body = '保存成功'
+  } catch(e) {
+    // console.log(e);
+    ctx.body = e.message
+  }
+  
+  // 存进去
+  
 }
-app.use(main)  //启用中间件
-app.listen(3000)
+app.use(main)
+app.listen(3000);
